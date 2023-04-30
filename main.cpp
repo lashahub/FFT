bfcd346fedfe4973686a4a213534c72a52f08b03
Lasha Koroshinadze <lashakoroshinadze199@gmail.com>
2023-05-01 01:57:54 +0200

    Added testing functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <complex>
 #include <vector>
+#include <random>
+
+#define NUM_SAMPLES 4096
+
+static std::random_device rd;
+using rand_eng = std::mt19937_64;
+static rand_eng eng(rd());
+static std::uniform_real_distribution<> uniform_real(0.0, 1.0);
+static std::uniform_int_distribution<> uniform_int(0);
 
 using complex = std::complex<double>;
 
@@ -17,15 +26,45 @@ public:
 
 };
 
-std::vector<complex> WavesToSamples(const std::vector<Wave> &waves, int sample_rate, double duration) {
-    std::vector<complex> samples(duration * sample_rate);
+std::vector<complex> waves_to_samples(const std::vector<Wave> &waves, int n_samples) {
+    std::vector<complex> samples(n_samples);
     for (size_t i = 0; i < samples.size(); ++i) {
         for (const auto &wave: waves) {
-            samples[i] += wave.amplitude * std::sin(2 * M_PI * wave.frequency * (double) i / (double) sample_rate +
+            samples[i] += wave.amplitude * std::sin(2 * M_PI * wave.frequency * (double) i / (double) n_samples +
                                                     wave.phase);
         }
     }
     return samples;
+}
+
+bool test(std::vector<complex> (*func)(const std::vector<complex> &)) {
+    int n_waves = 10;
+
+    std::vector<Wave> waves;
+    waves.reserve(n_waves);
+
+    for (int i = 0; i < n_waves; ++i) {
+        waves.emplace_back(uniform_int(eng) % (NUM_SAMPLES / 2),
+                           uniform_real(eng),
+                           uniform_real(eng));
+    }
+
+    std::vector<complex> samples = waves_to_samples(waves, NUM_SAMPLES);
+    std::vector<complex> X = func(samples);
+
+    for (int i = 0; i < n_waves; ++i) {
+        if (std::abs(waves[i].amplitude - std::abs(X[i])) > 0.0001) {
+            return false;
+        }
+        if (std::abs(waves[i].frequency - i) > 0.0001) {
+            return false;
+        }
+        if (std::abs(waves[i].phase - std::arg(X[i])) > 0.0001) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 std::vector<complex> dft(const std::vector<complex> &x) {
@@ -40,17 +79,12 @@ std::vector<complex> dft(const std::vector<complex> &x) {
 
 int main() {
 
-    std::vector<Wave> waves = {
-            {440,  1,    0},
-            {880,  0.5,  0},
-            {1320, 0.25, 0}
-    };
-
-
-
-    std::vector<complex> X = dft(WavesToSamples(waves, 44100, 0.1));
-    for (size_t i = 0; i < 500; ++i) {
-        std::cout << i << " " << std::norm(X[i]) << std::endl;
+    std::cout << "Testing DFT..." << std::endl;
+    if (test(dft)) {
+        std::cout << "DFT test passed!" << std::endl;
+    } else {
+        std::cout << "DFT test failed!" << std::endl;
     }
+
     return 0;
 }
