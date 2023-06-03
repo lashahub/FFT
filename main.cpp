71b7af225b4db2a51d7c774f238d1da95c45ebf6
dkorkot <dimitri.korkotashvili@polytechnique.edu>
2023-06-03 22:34:36 +0200

    implementation of ditfft for composite N. And parallel version of it together with 0 padding.

--- main.cpp
+++ main.cpp
@@ -2,36 +2,21 @@
 #include <complex>
 #include <cmath>
 #include <vector>
+#include <thread>
+
 
 using complex = std::complex<double>;
 
-class Wave {
-public:
-    double frequency;
-    double amplitude;
-    double phase;
-
-public:
-    Wave(double frequency, double amplitude, double phase)
-            : frequency(frequency), amplitude(amplitude),
-              phase(phase) {}
-
-};
-
-std::vector<complex> WavesToSamples(const std::vector<Wave> &waves, int sample_rate, double duration) {
-    std::vector<complex> samples(duration * sample_rate);
-    for (size_t i = 0; i < samples.size(); ++i) {
-        for (const auto &wave: waves) {
-            samples[i] += wave.amplitude * std::sin(2 * M_PI * wave.frequency * (double) i / (double) sample_rate +
-                                                    wave.phase);
-        }
+// helper function to join threads given thread vector
+void join_threads(std::vector<std::thread> &threads) {
+    for (auto &thread : threads) {
+        thread.join();
     }
-    return samples;
+    threads.clear();
 }
 
-
 // DFT function
-std::vector<complex> dft(const std::vector<complex> &x) {
+std::vector<complex> dft(std::vector<complex> &x) {
     std::vector<complex> X(x.size());
     for (int k = 0; k < x.size(); ++k) {
         for (int n = 0; n < x.size(); ++n) {
@@ -40,8 +25,6 @@ std::vector<complex> dft(const std::vector<complex> &x) {
     }
     return X;
 }
-
-
 
 // FFT function radix2-cooley-tukey
 std::vector<complex> fft(const std::vector<complex> &x) {
@@ -63,7 +46,7 @@ std::vector<complex> fft(const std::vector<complex> &x) {
 
     complex w = std::exp(complex(0, 0));
     complex w_n = std::exp(complex(0, -2 * M_PI / N));
-    for (int k = 0; k < x.size() / 2; ++k) {
+    for (int k = 0; k < x.size() / 2; k++) {
         X[k] = X_even[k] + w * X_odd[k];
         X[k + x.size() / 2] = X_even[k] - w * X_odd[k];
         w = w * w_n;
@@ -71,28 +54,254 @@ std::vector<complex> fft(const std::vector<complex> &x) {
     return X;
 }
 
+// get prime factors of N
+int get_prime_factors(int N) {
+    std::vector<int> factors;
+    int p = 2;
+    while (N > 1) {
+        if (N % p == 0) {
+            break;
+        }
+        p++;
+    }
+    return p;
+}
 
-//FFT General version for composite N = pq
-std::vector<complex> fft_general(const std::vector<complex> &x, int N, int p, int q) {
-    std::vector<complex> X(x.size());
+//DITFFT function which uses fft function
+std::vector<complex> ditfft(std::vector<complex> &P) {
+    int N = P.size();
+    int p = get_prime_factors(N);
+
+    if (p == N) {
+        return dft(P);
+    }
+
+    int q = N / p;
+    std::vector<std::vector<complex>> A(p, std::vector<complex>(q));
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < q; j++) {
+            A[i][j] = P[j * p + i];
+        }
+    }
+
+    std::vector<std::vector<complex>> B(p, std::vector<complex>(q));
+    for (int i = 0; i < p; ++i) {
+        B[i] = ditfft(A[i]);
+    }
+
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < q; j++) {
+            B[i][j] *= std::exp(complex(0, -2 * M_PI * i * j / N));
+        }
+    }
+
+    std::vector<std::vector<complex>> C(q, std::vector<complex>(p));
+    for (int i = 0; i < q; i++) {
+        for (int j = 0; j < p; j++) {
+            C[i][j] = B[j][i];
+        }
+    }
+
+    for (int i = 0; i < q; i++) {
+        C[i] = ditfft(C[i]);
+    }
+
+    std::vector<complex> P_star(N);
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < q; j++) {
+            P_star[i * q + j] = C[j][i];
+        }
+    }
+
+    return P_star;
+}
+
+//adjust size
+std::vector<complex> adjust_size(std::vector<complex> &P, int new_size) {
+    std::vector<complex> P_new(new_size);
+    for (int i = 0; i < P.size(); i++) {
+        P_new[i] = P[i];
+    }
+    return P_new;
+}
+
+
+//parallel ditfft
+std::vector<complex> parallel_ditfft(std::vector<complex> &P_OLD, int num_threads) {
+    std::vector<complex> P(P_OLD.size());
+    if (P_OLD.size() % num_threads != 0){
+        P = adjust_size(P_OLD, P_OLD.size() + num_threads - (P_OLD.size() % num_threads));
+    } else {
+        P = adjust_size(P_OLD, P_OLD.size());
+    }
+
+    std::cout << P_OLD.size() << " " << P.size() << std::endl;
+
+    int N = P.size();
+    int p = num_threads;
+    int q = N / p;
+
+    std::vector<std::thread> threads;
+
+    std::vector<std::vector<complex>> A(p, std::vector<complex>(q));
+    for (int i = 0; i < p - 1; i++) {
+        threads.push_back(std::thread([&, i]() {
+            for (int j = 0; j < q; j++) {
+                A[i][j] = P[j * p + i];
+            }
+        }));
+    }
+    for (int j = 0; j < q; j++) {
+        A[p - 1][j] = P[j * p + p - 1];
+    }
+
+    join_threads(threads);
+
+    std::vector<std::vector<complex>> B(p, std::vector<complex>(q));
+    for (int i = 0; i < p - 1; i++) {
+         threads.push_back(std::thread([&, i]() {
+            B[i] = ditfft(A[i]);
+         }));
+    }
+    B[p - 1] = ditfft(A[p - 1]);
+
+    join_threads(threads);
+
+    for (int i = 0; i < p-1; i++) {
+         threads.push_back(std::thread([&, i]() {
+            for (int j = 0; j < q; j++) {
+                B[i][j] *= std::exp(complex(0, -2 * M_PI * i * j / N));
+            }
+         }));
+    }
+    for (int j = 0; j < q; j++) {
+        B[p - 1][j] *= std::exp(complex(0, -2 * M_PI * (p - 1) * j / N));
+    }
+
+    join_threads(threads);
+
+    std::vector<std::vector<complex>> C(q, std::vector<complex>(p));
+    for (int i = 0; i < q; i++) {
+         threads.push_back(std::thread([&, i]() {
+            for (int j = 0; j < p; j++) {
+                C[i][j] = B[j][i];
+            }
+         }));
+    }
+
+    join_threads(threads);
+
+    for (int i = 0; i < q; i++) {
+        C[i] = ditfft(C[i]);
+    }
+
+    std::vector<complex> P_star(N);
+    for (int i = 0; i < p - 1; i++) {
+        threads.push_back(std::thread([&, i]() {
+            for (int j = 0; j < q; j++) {
+                P_star[i * q + j] = C[j][i];
+            }
+        }));
+    }
+    for (int j = 0; j < q; j++) {
+        P_star[(p - 1) * q + j] = C[j][p - 1];
+    }
+
+    join_threads(threads);
+
+    return P_star;
+}
+
+
+/*void fft(std::vector<std::complex<double>>& a, int start, int step, int size) {
+    if (size < 2) {
+        return;
+    }
+
+    // compute half-size FFTs
+    fft(a, start, 2*step, size/2);
+    fft(a, start+step, 2*step, size/2);
+
+    // combine results
+    for (int i = 0; i < size/2; i++) {
+        std::complex<double> t = std::exp(std::complex<double>(0, -2.0 * PI * i / size)) * a[start + 2*step*i + step];
+        a[start + 2*step*i + step] = a[start + 2*step*i] - t;
+        a[start + 2*step*i] += t;
+    }
+}
+
+void mixedRadixFft(std::vector<std::complex<double>>& a, std::vector<int>& factors) {
+    int N = a.size();
+    int product = 1;
+
+    for (int factor : factors) {
+        int step = N / (product * factor);
+        for (int i = 0; i < product; i++) {
+            fft(a, step*i, step, factor);
+        }
+        product *= factor;
+    }
+}*/
+
+// Function that generates random polynomial coefficients and compares the results of dft and ditfft
+std::pair<bool, int> test(std::vector<complex> (*fun1)(std::vector<complex> &),
+                          std::vector<complex> (*fun2)(std::vector<complex> &), int N) {
+    std::vector<complex> P(N);
+    for (int i = 0; i < N; ++i) {
+        int r = rand();
+        int im = rand();
+        P[i] = complex(r % 10, im % 10);
+    }
+    bool passed = true;
+    int counter = 0;
+    std::vector<complex> X = fun1(P);
+    std::vector<complex> Y = fun2(P);
+    for (int i = 0; i < X.size(); ++i) {
+        if (std::abs(X[i] - Y[i]) > 0.001) {
+            counter++;
+            std::cout << "Failed!" << std::endl;
+            passed = false;
+        }
+    }
+    return {passed, counter};
+}
+
+// function for testing for M times for given functions
+void test_for_M_times(std::vector<complex> (*fun1)(std::vector<complex> &),
+                      std::vector<complex> (*fun2)(std::vector<complex> &), int M, int N) {
+    int passed = 0;
+    for (int i = 0; i < M; ++i) {
+        std::pair<bool, int> result = test(fun1, fun2, N);
+        passed += result.first;
+    }
+    std::cout << "Passed " << passed << "/" << M << " tests" << std::endl;
 }
 
 
 int main() {
 
-    std::vector<Wave> waves = {
-            {440,  1,    0},
-            {880,  0.5,  0},
-            {1320, 0.25, 0}
-    };
+    //test_for_M_times(dft, ditfft, 100, 512);
+    //test_for_M_times(dft, fft, 100, 512);
 
-    std::vector<complex> X = dft(WavesToSamples(waves, 4410, 0.2322));
-    std::vector<complex> X2 = fft(WavesToSamples(waves, 4410, 0.2322));
-    std::cout << X.size() << std::endl;
-    for (size_t i = 0; i < 500; ++i) {
-        std::cout << i << " " << std::norm(X2[i] - X[i]) << std::endl;
+    int N = 3;
+    std::vector<complex> P(N);
+    for (int i = 0; i < N; i++) {
+        // generate a random integer
+        int r = i;
+        int im = 0;
+        P[i] = complex(r, im);
     }
-    std:: cout << X.size() << std::endl;
+
+//    std::vector<complex> X = parallel_ditfft(P, 5);
+    P.push_back(complex(0, 0));
+//    P.push_back(complex(0, 0));
+//    P.push_back(complex(0, 0));
+//    P.push_back(complex(0, 0));
+
+    std::vector<complex> Y = dft(P);
+    for (int i = 0; i < Y.size(); i++) {
+        std::cout << Y[i] << std::endl;
+    }
     return 0;
 }
 
