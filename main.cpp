575203d349b2412920c2c7babb0c03ed746793ef
Lasha Koroshinadze <lashakoroshinadze199@gmail.com>
2023-06-06 15:53:50 +0200

    Contour recognition in an image + FFT and IFFT

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-#include <vector>
-#include <complex>
-#include <cmath>
-#include <iostream>
-#include <thread>
-#include <algorithm>
-#include <random>
-
 #define STB_IMAGE_IMPLEMENTATION
 
 #include "stb_image.h"
@@ -16,107 +8,9 @@
 
 #include "utils.hpp"
 
-const int MAX_THREAD_DEPTH = 4;
-using complex = std::complex<double>;
-
 std::random_device rd;
 std::uniform_int_distribution<> uniform(0, std::numeric_limits<int>::max());
 std::mt19937_64 engine(rd());
-
-size_t next_power_of_2(size_t n) {
-    size_t res = 1;
-    while (res < n) {
-        res <<= 1;
-    }
-    return res;
-}
-
-void fft_rec(std::vector<complex> &a, int depth = 0) {
-    size_t n = a.size();
-    if (n == 1)
-        return;
-
-    std::vector<complex> even(n / 2), odd(n / 2);
-
-    if (depth < MAX_THREAD_DEPTH) {
-#pragma omp parallel for
-        for (int i = 0; i < n / 2; i++) {
-            even[i] = a[2 * i];
-            if (2 * i + 1 < n) {
-                odd[i] = a[2 * i + 1];
-            }
-        }
-    } else {
-        for (int i = 0; 2 * i < n; i++) {
-            even[i] = a[2 * i];
-            if (2 * i + 1 < n) {
-                odd[i] = a[2 * i + 1];
-            }
-        }
-    }
-
-    if (depth < MAX_THREAD_DEPTH) {
-        std::thread t1(fft_rec, std::ref(even), depth + 1);
-        std::thread t2(fft_rec, std::ref(odd), depth + 1);
-        t1.join();
-        t2.join();
-    } else {
-        fft_rec(even, depth + 1);
-        fft_rec(odd, depth + 1);
-    }
-
-    double ang = 2 * M_PI / (double) n;
-
-
-    if (depth < MAX_THREAD_DEPTH) {
-#pragma omp parallel for
-        for (size_t i = 0; i < n / 2; i++) {
-            complex t(std::cos((double) i * ang), (std::sin((double) i * ang)));
-            a[i] = even[i] + t * odd[i];
-            a[i + n / 2] = even[i] - t * odd[i];
-        }
-    } else {
-        for (size_t i = 0; 2 * i < n; i++) {
-            complex t(std::cos((double) i * ang), (std::sin((double) i * ang)));
-            a[i] = even[i] + t * odd[i];
-            a[i + n / 2] = even[i] - t * odd[i];
-        }
-    }
-}
-
-void fft(std::vector<complex> &a) {
-    size_t n = a.size();
-    a.resize(next_power_of_2(n));
-    fft_rec(a);
-    a.resize(n);
-}
-
-std::vector<complex> dft(const std::vector<complex> &a) {
-    size_t length = a.size();
-    size_t n = next_power_of_2(length);
-    std::vector<complex> y(length);
-#pragma omp parallel for
-    for (size_t k = 0; k < length; k++) {
-        for (size_t j = 0; j < length; j++) {
-            double angle = 2 * M_PI * (double) j * (double) k / (double) n;
-            y[k] += a[j] * complex(std::cos(angle), std::sin(angle));
-        }
-    }
-    return y;
-}
-
-bool compareResults(const std::vector<complex> &fft_res, const std::vector<complex> &dft_res) {
-    double epsilon = 1e-2;
-    if (fft_res.size() != dft_res.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < fft_res.size(); i++) {
-        if (std::abs(fft_res[i] - dft_res[i]) > epsilon) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main() {
     int width, height, channels;
@@ -128,19 +22,23 @@ int main() {
         return -1;
     }
 
-    uint8_t *gray_img = new uint8_t[width * height];
-    for (int i = 0; i < width * height * channels; i += channels) {
-        uint8_t gray_value = 0.299 * img[i] + 0.587 * img[i + 1] + 0.114 * img[i + 2];
-        gray_img[i / channels] = gray_value;
+    Point start = findStartingPoint(img, width, height, channels);
+    std::vector<Point> contour = traceContour(img, width, height, channels, start);
+    std::vector<Point> transformedContour = transformContour(contour);
+
+    auto *output_img = new uint8_t[width * height * channels];
+    memset(output_img, 255, width * height * channels);
+    for (const Point &point: transformedContour) {
+        if (point.x >= 0 && point.x < width && point.y >= 0 && point.y < height) {
+            output_img[(point.y * width + point.x) * channels + 0] = 0;
+            output_img[(point.y * width + point.x) * channels + 1] = 0;
+            output_img[(point.y * width + point.x) * channels + 2] = 0;
+        }
     }
+    stbi_write_png("output.png", width, height, channels, output_img, width * channels);
 
     stbi_image_free(img);
-
-    std::vector<uint8_t> edge_detected_image = apply_sobel(gray_img, width, height);
-
-    stbi_write_png("edge_detected_image.png", width, height, 1, edge_detected_image.data(), width);
-
-    delete[] gray_img;
+    delete[] output_img;
 
     return 0;
 }
--- utils.hpp
+++ utils.hpp
@@ -1,7 +1,37 @@
 #pragma once
 
 #include <vector>
-#include <cstdint>
+#include <complex>
 #include <cmath>
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <random>
 
-std::vector<uint8_t> apply_sobel(uint8_t *img, int width, int height);
+const size_t MAX_THREAD_DEPTH = 4;
+using complex = std::complex<double>;
+
+extern std::random_device rd;
+extern std::uniform_int_distribution<> uniform;
+extern std::mt19937_64 engine;
+
+struct Point {
+    int x;
+    int y;
+};
+
+size_t next_power_of_2(size_t n);
+
+void fft_rec(std::vector<complex> &a, size_t depth = 0);
+
+void fft(std::vector<complex> &a);
+
+void ifft(std::vector<complex> &a);
+
+std::vector<complex> dft(const std::vector<complex> &a);
+
+Point findStartingPoint(const uint8_t *image, int width, int height, int channels);
+
+std::vector<Point> traceContour(const uint8_t *image, int width, int height, int channels, Point start);
+
+std::vector<Point> transformContour(const std::vector<Point> &contour);
